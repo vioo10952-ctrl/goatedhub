44ce2affeff5306dc76b722970648c55d667e226
Armeuim Skript <vioo10952@gmail.com>
2026-04-26 02:07:27 +0200

    Update goatedhub.lua

--- goatedhub.lua
+++ goatedhub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://encrypt-x.pages.dev/Scripts?Id=2150472169692"))(2150472169692)
+loadstring(game:HttpGet("https://encrypt-x.pages.dev/Scripts?Id=3633171612277"))("3633171612277")
